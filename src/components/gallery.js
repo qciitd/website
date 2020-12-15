60bb7380c4816a08eb971e74cbe5988596424c1c
rehan-bhatia <bhatia.rehan4@icloud.com>
2020-12-15 16:10:50 +0530

    adding design change in hall of fame

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import { isBrowser } from "react-device-detect";
+import { BrowserView, MobileView } from "react-device-detect";
 import Modal from "../components/modal.js"
 
 class ImageGallery extends React.Component {
@@ -27,25 +27,41 @@ class ImageGallery extends React.Component {
 		console.log(this.state)
 		return(
 			<div>
-				<main class="cf pa2">
-				    <div class="fl w-80 w-40-ns ph2 tc">
-				    	<p class="pv2 grow db no-underline black pointer"><img onClick={(e)=>this.showModal(e)} alt="hello1" class="db w-100" src="https://s3-us-west-2.amazonaws.com/prnt/elevaters030211_960.jpg"/></p>
-					    <p class="no-underline pv2 grow db pointer"><img onClick={(e)=>this.showModal(e)} class="db w-100" alt="hello2" src="https://s3-us-west-2.amazonaws.com/prnt/elevaters.jpg"/></p>
-					    <p class="no-underline pv2 grow db pointer"><img onClick={(e)=>this.showModal(e)} class="db w-100" alt="hello3" src="https://s3-us-west-2.amazonaws.com/prnt/pink-and-noseworthy-22.12.10-cargo_960.jpg"/></p>
-				    </div>
-				    <div class="fl w-60 w-30-ns ph2 tc">
-				    	<p class="no-underline pv2 grow db pointer"><img onClick={(e)=>this.showModal(e)} class="db w-100" alt="hello2" src="https://s3-us-west-2.amazonaws.com/prnt/elevaters.jpg"/></p>
-					    <p class="no-underline pv2 grow db pointer"><img onClick={(e)=>this.showModal(e)} class="db w-100" alt="hello3" src="https://s3-us-west-2.amazonaws.com/prnt/pink-and-noseworthy-22.12.10-cargo_960.jpg"/></p>
-				    	<p class="pv2 grow db no-underline black pointer"><img onClick={(e)=>this.showModal(e)} alt="hello1" class="db w-100" src="https://s3-us-west-2.amazonaws.com/prnt/elevaters030211_960.jpg"/></p>
-					    <p class="pv2 grow db no-underline black pointer"><img onClick={(e)=>this.showModal(e)} class="db w-100" alt="hello4" src="https://s3-us-west-2.amazonaws.com/prnt/hw090911_960.jpg"/></p>
-				    </div>
-				    <div class="fl w-60 w-30-ns ph2 tc">
-				    	<p class="no-underline pv2 grow db pointer"><img onClick={(e)=>this.showModal(e)} class="db w-100" alt="hello3" src="https://s3-us-west-2.amazonaws.com/prnt/pink-and-noseworthy-22.12.10-cargo_960.jpg"/></p>
-					    <p class="pv2 grow db no-underline black pointer"><img onClick={(e)=>this.showModal(e)} class="db w-100" alt="hello4" src="https://s3-us-west-2.amazonaws.com/prnt/hw090911_960.jpg"/></p>
-				    	<p class="pv2 grow db no-underline black pointer"><img onClick={(e)=>this.showModal(e)} alt="hello1" class="db w-100" src="https://s3-us-west-2.amazonaws.com/prnt/elevaters030211_960.jpg"/></p>
-					    <p class="no-underline pv2 grow db pointer"><img onClick={(e)=>this.showModal(e)} class="db w-100" alt="hello2" src="https://s3-us-west-2.amazonaws.com/prnt/elevaters.jpg"/></p>
-				    </div>
-				</main>
+				<BrowserView>
+					<main class="cf pa2">
+					    <div class="fl w-80 w-40-ns ph2 tc">
+					    	<p class="pv2 grow db no-underline black pointer"><img onClick={(e)=>this.showModal(e)} alt="hello1" class="db w-100" src="https://s3-us-west-2.amazonaws.com/prnt/elevaters030211_960.jpg"/></p>
+						    <p class="no-underline pv2 grow db pointer"><img onClick={(e)=>this.showModal(e)} class="db w-100" alt="hello2" src="https://s3-us-west-2.amazonaws.com/prnt/elevaters.jpg"/></p>
+						    <p class="no-underline pv2 grow db pointer"><img onClick={(e)=>this.showModal(e)} class="db w-100" alt="hello3" src="https://s3-us-west-2.amazonaws.com/prnt/pink-and-noseworthy-22.12.10-cargo_960.jpg"/></p>
+					    </div>
+					    <div class="fl w-60 w-30-ns ph2 tc">
+					    	<p class="no-underline pv2 grow db pointer"><img onClick={(e)=>this.showModal(e)} class="db w-100" alt="hello2" src="https://s3-us-west-2.amazonaws.com/prnt/elevaters.jpg"/></p>
+						    <p class="no-underline pv2 grow db pointer"><img onClick={(e)=>this.showModal(e)} class="db w-100" alt="hello3" src="https://s3-us-west-2.amazonaws.com/prnt/pink-and-noseworthy-22.12.10-cargo_960.jpg"/></p>
+					    	<p class="pv2 grow db no-underline black pointer"><img onClick={(e)=>this.showModal(e)} alt="hello1" class="db w-100" src="https://s3-us-west-2.amazonaws.com/prnt/elevaters030211_960.jpg"/></p>
+						    <p class="pv2 grow db no-underline black pointer"><img onClick={(e)=>this.showModal(e)} class="db w-100" alt="hello4" src="https://s3-us-west-2.amazonaws.com/prnt/hw090911_960.jpg"/></p>
+					    </div>
+					    <div class="fl w-60 w-30-ns ph2 tc">
+					    	<p class="no-underline pv2 grow db pointer"><img onClick={(e)=>this.showModal(e)} class="db w-100" alt="hello3" src="https://s3-us-west-2.amazonaws.com/prnt/pink-and-noseworthy-22.12.10-cargo_960.jpg"/></p>
+						    <p class="pv2 grow db no-underline black pointer"><img onClick={(e)=>this.showModal(e)} class="db w-100" alt="hello4" src="https://s3-us-west-2.amazonaws.com/prnt/hw090911_960.jpg"/></p>
+					    	<p class="pv2 grow db no-underline black pointer"><img onClick={(e)=>this.showModal(e)} alt="hello1" class="db w-100" src="https://s3-us-west-2.amazonaws.com/prnt/elevaters030211_960.jpg"/></p>
+						    <p class="no-underline pv2 grow db pointer"><img onClick={(e)=>this.showModal(e)} class="db w-100" alt="hello2" src="https://s3-us-west-2.amazonaws.com/prnt/elevaters.jpg"/></p>
+					    </div>
+					</main>
+				</BrowserView>
+				<MobileView>
+					<div class="fl w-50 w-25-ns ph2 tc">
+					   	<p class="no-underline pv2 grow db pointer"><img onClick={(e)=>this.showModal(e)} class="db w-100" alt="hello2" src="https://s3-us-west-2.amazonaws.com/prnt/elevaters.jpg"/></p>
+						<p class="no-underline pv2 grow db pointer"><img onClick={(e)=>this.showModal(e)} class="db w-100" alt="hello3" src="https://s3-us-west-2.amazonaws.com/prnt/pink-and-noseworthy-22.12.10-cargo_960.jpg"/></p>
+					  	<p class="pv2 grow db no-underline black pointer"><img onClick={(e)=>this.showModal(e)} alt="hello1" class="db w-100" src="https://s3-us-west-2.amazonaws.com/prnt/elevaters030211_960.jpg"/></p>
+						<p class="pv2 grow db no-underline black pointer"><img onClick={(e)=>this.showModal(e)} class="db w-100" alt="hello4" src="https://s3-us-west-2.amazonaws.com/prnt/hw090911_960.jpg"/></p>
+					</div>
+					<div class="fl w-50 w-25-ns ph2 tc">
+						<p class="no-underline pv2 grow db pointer"><img onClick={(e)=>this.showModal(e)} class="db w-100" alt="hello3" src="https://s3-us-west-2.amazonaws.com/prnt/pink-and-noseworthy-22.12.10-cargo_960.jpg"/></p>
+						<p class="pv2 grow db no-underline black pointer"><img onClick={(e)=>this.showModal(e)} class="db w-100" alt="hello4" src="https://s3-us-west-2.amazonaws.com/prnt/hw090911_960.jpg"/></p>
+					    <p class="pv2 grow db no-underline black pointer"><img onClick={(e)=>this.showModal(e)} alt="hello1" class="db w-100" src="https://s3-us-west-2.amazonaws.com/prnt/elevaters030211_960.jpg"/></p>
+						<p class="no-underline pv2 grow db pointer"><img onClick={(e)=>this.showModal(e)} class="db w-100" alt="hello2" src="https://s3-us-west-2.amazonaws.com/prnt/elevaters.jpg"/></p>
+					</div>
+				</MobileView>
 				<Modal show={this.state.show} handleClose={this.hideModal} link={this.state.link} caption = {this.state.caption}>
 		          <p>Modal</p>
 		          <p>Data</p>
