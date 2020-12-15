19b0023ac576a102c5510c0d567fe9b109edb093
rehan-bhatia <bhatia.rehan4@icloud.com>
2020-12-15 15:54:04 +0530

    adding design change in hall of fame

--- src/components/modal.js
+++ src/components/modal.js
@@ -4,14 +4,20 @@ import "./modal.css"
 const Modal = ({ handleClose, show, link, caption }) => {
   const showHideClassName = show ? "modal display-block" : "modal display-none";
 
+  const handleClosing = (e) => {
+    console.log(e.target.tagName)
+    if (e.target.tagName != "IMG") {
+      handleClose()
+    }
+  }
+
   return (
-    <div className={showHideClassName}>
-      <section className="modal-main tc">
-        <div class="center tc">
-        	<img src={link} style={{"z-index": "-1", maxHeight:"90vh", margin: "auto"}}/>
-        	<p class="pointer dim white avenir" style={{position: "fixed", top: "0px", right:"0"}} onClick={()=>handleClose()}>X</p>	
-        	<div class="w-100 tc" style={{height:"10px"}}>
-        		<p class="f6 avenir br3 ph3 pv2 mt1 dib white" style={{background: "#4bb069"}}>{caption}</p>
+    <div onClick={(e)=>handleClosing(e)} className={showHideClassName}>
+      <section onClick={(e)=>handleClosing(e)} className="modal-main tc">
+        <div onClick={(e)=>handleClosing(e)} class="center tc">
+        	<img onClick={(e)=>handleClosing(e)} src={link} style={{"z-index": "-1", maxHeight:"90vh", margin: "auto"}}/>
+        	<div onClick={(e)=>handleClosing(e)} class="w-100 tc" style={{height:"10px"}}>
+        		<p onClick={(e)=>handleClosing(e)} class="f6 avenir br3 ph3 pv2 mt1 dib white" style={{background: "#4bb069"}}>{caption}</p>
         	</div>
         </div>
       </section>
