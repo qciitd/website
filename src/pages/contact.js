d10e46547b1b1731838ff91ea9a06e5e660b9549
Om Krishna <omkrishna.ok@gmail.com>
2021-01-16 21:46:20 +0530

    google form redesign

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -33,96 +33,33 @@ class ContactPage extends React.Component {
     return (
       <div style={{ margin: "0 auto", fontFamily: "Raleway" }}>
         <Nav />
-        <div
-          className="bg-green cream-text container75"
-          style={{ fontFamily: "Raleway" }}
-        >
-          <div className="contact-text-container">
-            <h1
-              className={`lh-copy ${isBrowser ? "w-50" : "tc"}`}
-              style={{
-                fontSize: "1.5em",
-                textAlign: "center",
-                marginLeft: "auto",
-              }}
-            >
-              We answer your questions for a living (literally). Feel free to
-              get in touch by filling this form
-            </h1>
+        <div className="bg-green container-75">
+          <div className="contact-form-text">
+            We answer your questions for a living (literally). Feel free to get
+            in touch by filling this form
           </div>
-          <div className="google-form-container">
-            <iframe
-              id="google-form"
-              src="https://docs.google.com/forms/d/e/1FAIpQLSc0kbyzMwJwPV_5OF8CGpWRra1v9boFl5G6NziigkuhgrzP6w/viewform?embedded=true"
-              width="370"
-              height="640"
-              frameborder="0"
-            >
-              Loading…
-            </iframe>
-          </div>
+          <div className="contact-form">
+            <form action="https://docs.google.com/forms/u/2/d/e/1FAIpQLSc0kbyzMwJwPV_5OF8CGpWRra1v9boFl5G6NziigkuhgrzP6w/formResponse">
+              <input
+                className="contact-form-input"
+                type="text"
+                name="entry.31754825"
+                placeholder="Your Name (required)"
+              ></input>
+              <input
+                className="contact-form-input"
+                type="text"
+                name="entry.461730877"
+                placeholder="Your Email (required)"
+              ></input>
 
-          {/* <div className="contact-form-container">
-            <Formik
-              initialValues={{
-                name: "",
-                email: "",
-                message: "",
-              }}
-              onSubmit={(values, actions) => {
-                fetch("/", {
-                  method: "POST",
-                  headers: {
-                    "Content-Type": "application/x-www-form-urlencoded",
-                  },
-                  body: encode({ "form-name": "qciitd-contact", ...values }),
-                })
-                  .then(() => {
-                    alert("Success");
-                    actions.resetForm();
-                  })
-                  .catch(() => {
-                    alert("Error");
-                  })
-                  .finally(() => actions.setSubmitting(false));
-              }}
-              validate={(values) => {
-                const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-                const errors = {};
-                if (!values.name) {
-                  errors.name = "Name Required";
-                }
-                if (!values.email || !emailRegex.test(values.email)) {
-                  errors.email = "Valid Email Required";
-                }
-                if (!values.message) {
-                  errors.message = "Message Required";
-                }
-                return errors;
-              }}
-            >
-              {() => (
-                <Form name="qciitd-contact" data-netlify={true}>
-                  <label htmlFor="name">Name: </label>
-                  <Field name="name" />
-                  <label htmlFor="email">Email: </label>
-                  <Field name="email" />
-                  <label htmlFor="message">Message: </label>
-                  <Field name="message" component="textarea" />
-                  <button type="submit">Send</button>
-                  <p>
-                    <ErrorMessage name="name" />
-                  </p>
-                  <p>
-                    <ErrorMessage name="email" />{" "}
-                  </p>
-                  <p>
-                    <ErrorMessage name="message" />
-                  </p>
-                </Form>
-              )}
-            </Formik>
-          </div> */}
+              <textarea
+                name="entry.669084690"
+                placeholder="Your Message (required)"
+              ></textarea>
+              <button>Submit</button>
+            </form>
+          </div>
         </div>
         <Footer />
       </div>
