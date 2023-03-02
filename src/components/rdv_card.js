7998fde3b510d55364e2ed9beefefb48215a0d78
qciitd <74850997+qciitd@users.noreply.github.com>
2023-03-02 20:51:30 +0530

    Update rdv_card.js
    Removed Cash prize mention

--- src/components/rdv_card.js
+++ src/components/rdv_card.js
@@ -8,7 +8,7 @@ const RDVCard = ({event, qm, date, time, teamsize, link, image}) => {
 			<p className="f2 ph3 mv0 pv0">{event}</p>
 			<p className="f4 ph3 mt3 mb0">QM: {qm}</p>
 			<p className="f4 ph3 mt3 mb0">{date}, at {time}</p>
-			<p className="f4 ph3 mt4">Cash prizes worth <span class="f3">48K</span></p>
+			//<p className="f4 ph3 mt4">Cash prizes worth <span class="f3">48K</span></p>
 			<p className="f4 ph3 mv1">Teams of {teamsize}</p>
 			<p className="f5 w-70 ph3 mt1 mb4">All participants must be under 25 years of age</p>
 			<a class="f5 ph3 white underline pointer dim" href={link} target='_blank'>Register Now!</a>
